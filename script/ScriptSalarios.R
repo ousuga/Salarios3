b0035e216b1f927b2b072016de9f30a3ceaa261c
ousuga <ousuga@gmail.com>
2023-05-24 19:34:29 -0500

    Generación de script

---
+++ script/ScriptSalarios.R
@@ -0,0 +1,6 @@
+# Gráfico salarios vs dos categorias
+library(ggplot2)
+ggplot(salarios, aes(x = work_year, y = salary_in_usd, fill = experience_level,
+                     col = experience_level)) +
+  geom_boxplot(alpha = 0.4)
+
