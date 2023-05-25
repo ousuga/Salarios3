19dc0f4ec383ad6a6b668a5d372c6d537a9ece0f
ousuga <ousuga@gmail.com>
2023-05-24 19:48:09 -0500

    Adición de comentario

--- script/ScriptSalarios.R
+++ script/ScriptSalarios.R
@@ -4,3 +4,4 @@ ggplot(salarios, aes(x = work_year, y = salary_in_usd, fill = experience_level,
                      col = experience_level)) +
   geom_boxplot(alpha = 0.4)
 
+# Comentario adicional 
